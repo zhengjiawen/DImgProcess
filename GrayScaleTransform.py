f389198ed58c7e72ed07140f6b59ea97a69de59d
zhengjiawen <756848513@qq.com>
2018-09-23 12:56:40 +0800

    Initial commit

---
+++ GrayScaleTransform.py
@@ -0,0 +1,167 @@
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+
+def imgGrayReverse(img):
+    '''
+    灰度图反色
+    :param img:narray对象，灰度图是二维的
+    :return: 反色后的narray对象
+    '''
+    height, width = img.shape
+    temp = img.copy()
+
+    for i in range(height):
+        for j in range(width):
+            temp[i, j] = 255 - img[i, j]
+
+    return temp
+
+def imgColorReverse(img):
+    '''
+    灰度图反色
+    :param img:narray对象，RGB图是三维的
+    :return: 反色后的narray对象
+    '''
+    height, width ,channel= img.shape
+    temp = img.copy()
+
+    for i in range(height):
+        for j in range(width):
+            temp[i, j] = 255 - img[i, j]
+
+    return temp
+
+def imgReverse(img):
+    '''
+    灰度图反色
+    :param img:narray对象，灰度图是二维的,RGB图是三维的
+    :return: 反色后的narray对象
+    '''
+    dimension = len(img.shape)
+    if dimension == 2:
+        height, width = img.shape
+    elif dimension == 3:
+        height, width, channel = img.shape
+    else:
+        raise(RuntimeError("维度错误,维度为"+str(dimension)))
+
+    temp = img.copy()
+
+    for i in range(height):
+        for j in range(width):
+            temp[i, j] = 255 - img[i, j]
+
+    return temp
+
+def imgLogProcess(img, c=1):
+    '''
+    对数变换
+    :param img: narray对象，灰度图是二维的,RGB图是三维的
+            c:  常数
+    :return: log变换后的narray对象
+    '''
+    dimension = len(img.shape)
+    if dimension == 2:
+        height, width = img.shape
+    elif dimension == 3:
+        height, width, channel = img.shape
+    else:
+        raise(RuntimeError("维度错误,维度为"+str(dimension)))
+    temp = img.copy()
+
+    for i in range(height):
+        for j in range(width):
+            temp[i, j] = c*np.log(1+img[i, j])
+
+    return temp
+
+def imgGamaProcess(img, c=1, gama=1):
+    '''
+    伽马变换
+    :param img: narray对象，灰度图是二维的,RGB图是三维的
+            c:  常数
+    :return: log变换后的narray对象
+    '''
+    dimension = len(img.shape)
+    if dimension == 2:
+        height, width = img.shape
+    elif dimension == 3:
+        height, width, channel = img.shape
+    else:
+        raise(RuntimeError("维度错误,维度为"+str(dimension)))
+    temp = img.copy()
+
+    for i in range(height):
+        for j in range(width):
+            temp[i, j] = c*np.power(img[i, j], gama)
+
+    return temp
+
+def showHist(img):
+    '''
+    展示直方图
+    :param img: narray对象，灰度图是二维的,RGB图是三维的
+    :return: 
+    '''
+    dimension = len(img.shape)
+    if dimension == 2:
+        showGrayHist(img)
+    elif dimension == 3:
+        showColorHist(img)
+    else:
+        raise(RuntimeError("维度错误,维度为"+str(dimension)))
+
+def showGrayHist(img):
+    '''
+    展示灰度图的直方图
+    :param img: narray对象，灰度图是二维的,RGB图是三维的
+    :return: 
+    '''
+    hist = cv.calcHist([img], [0], None, [256], [0, 255])
+    plt.figure()
+    plt.title("GrayScale Hist")
+    plt.xlabel("Bins")
+    plt.ylabel("num of Pixels")
+    plt.plot(hist)
+    plt.xlim([0, 255])
+    plt.show()
+
+def showColorHist(img):
+    '''
+    展示RGB图的直方图
+    :param img: narray对象，灰度图是二维的,RGB图是三维的
+    :return: 
+    '''
+
+    channels = cv.split(img)
+    colors = ("b", "g", "r")
+    plt.figure()
+    plt.title("ColorScale Hist")
+    plt.xlabel("Bins")
+    plt.ylabel("num of Pixels")
+    for (channel, color) in zip(channels, colors):
+        hist = cv.calcHist([channel], [0], None, [256], [0, 255])
+        plt.plot(hist, color = color)
+        plt.xlim([0, 255])
+    plt.show()
+
+
+
+
+img = cv.imread("F:/lena.jpg",0)
+eq = cv.equalizeHist(img)
+
+cv.imshow("Histgram Equalization", np.hstack([img,eq]))
+cv.waitKey(0)
+showHist(img)
+showHist(eq)
+
+
+# test = imgGamaProcess(img,10,0.2)
+# cv.namedWindow("test")
+# cv.imshow("test", test)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
+
